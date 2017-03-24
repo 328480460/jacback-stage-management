30260f9ee08f4e808acb389e595f94f3e67d57b4
zhangtao <328480460@qq.com>
2017-03-24 14:51:46 +0800

    updata

--- src/vuex/store.js
+++ src/vuex/store.js
@@ -2,13 +2,13 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 import state from './state';
 import mutations from './mutations';
-// import actions from './actions';
-// import getters from './getters';
+import actions from './actions';
+import getters from './getters';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state,
   mutations,
-  // actions,
-  // getters
+  actions,
+  getters
 });
